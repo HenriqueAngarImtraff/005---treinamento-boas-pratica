7fb288037c311324d18c15ab4fdb5a17df3d0add
Henrique Angar <henrique.angar@grupoimtraff.com.br>
2023-02-03 15:56:25 -0300

    Revert "MODIFY - code changes"
    This reverts commit b4aaec10517d83991ecbfdc492cae88ed16981c2.

--- main.py
+++ main.py
@@ -4,15 +4,45 @@
 # Renomeie o código para nomes que indiquem as funcionalidades
 # Insira comentários sobre as funções
 
-import get_data
-import process_data
+import pandas as pd
 
 
 def main():
-    df = get_data.imp()
-    df = process_data.prd(df)
+    df = imp()
+    df = prd(df)
     df.to_excel('RESUMO.xlsx')
 
 
-def not_necessary():
-    return False
+# compõe a parte de processamento dos dados
+
+
+def prd(data):
+    data['area'] = caca(data['xi'], data['xf'], data['yi'], data['yf'])
+    info = orgi(data)
+
+    return info
+
+
+def caca(xi, xf, yi, yf):
+    # renomeie para calc_area
+    # separe essa função de modo a operação ser feita em função a parte
+    # altere a função para (value*3.5)/960
+    xi = (xi*3)/96
+    xf = (xf*3)/96
+    yi = (yi*3)/96
+    yf = (yf*3)/96
+
+    area = (xi - xf)*(yi - yf)
+    return area
+
+
+def orgi(info):
+
+    # renomeie para organize_info
+    # Descomente a parte do código que foi desativada
+    info.drop(columns=['width', 'height', 'xi',
+              'xf', 'yi', 'yf'], inplace=True)
+
+    info = info.groupby('defect').sum('area')
+    # info = info.sort_values(by='area', ascending=False)
+    return info
